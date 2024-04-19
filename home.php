5b791b6440bf2b1906c3a8919c983f250ebacaba
AdarshStalu <200564265@student.georgianc.on.ca>
2024-04-19 12:47:36 -0400

    creating a css for my home page

--- home.php
+++ home.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="./css/common.css" /> 
+<link rel="stylesheet" href="./css/home.css" /> 
 <?php 
     $title = 'Home';
     include('shared/header.php'); 
     
 ?>
-    <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sports Team Manager</h1>
-    <br>
+<body>
     <br>
     <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Make Your Dream Football Team. Login To Your Account For Adding, Editing Or Removing Players From Our Database</h2>
     <p> This website allows users to design their dream food team by adding players. You can add players to our database after logging into your account and start creating your team. Users also have an opportunity to edit their team and upload players to this website. This website is created as a part of my PHP Assignment 2. Assignment 1 is the base of this project. Apart from this, CSS styling is added to this website as a part of my project 1 in CSS. </p>
 
 
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Responsive navbar from <a href="https://www.aleksandrhovhannisyan.com/blog/responsive-navbar-tutorial/" target="_blank">
+    <p>Responsive navbar from <a href="https://www.aleksandrhovhannisyan.com/blog/responsive-navbar-tutorial/" target="_blank">
     https://www.aleksandrhovhannisyan.com/blog/responsive-navbar-tutorial/</a></p>
 </main>
 </body>
