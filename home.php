313ad7ca392c3c6291ef52ab6d85e9af1aa2efe7
AdarshStalu <200564265@student.georgianc.on.ca>
2024-04-19 12:59:52 -0400

    Adding a back face visibility on the home page css

--- home.php
+++ home.php
@@ -8,6 +8,18 @@
     
 ?>
 <body>
+
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <h1>Sports Team Manager</h1>
+    </div>
+    <div class="flip-card-back">
+      <h1>CSS Project 1- Adarsh Stalu</h1>
+    </div>
+  </div>
+</div>
+
     <br>
     <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Make Your Dream Football Team. Login To Your Account For Adding, Editing Or Removing Players From Our Database</h2>
     <p> This website allows users to design their dream food team by adding players. You can add players to our database after logging into your account and start creating your team. Users also have an opportunity to edit their team and upload players to this website. This website is created as a part of my PHP Assignment 2. Assignment 1 is the base of this project. Apart from this, CSS styling is added to this website as a part of my project 1 in CSS. </p>
